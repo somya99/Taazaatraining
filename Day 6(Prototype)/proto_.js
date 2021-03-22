acf14a38ee25f4b6d757e73e42c17ff99747acb7
somya99 <dassomya1999@gmail.com>
2021-03-22 07:20:38 +0530

    DAY 6 assignment

---
+++ Day 6(Prototype)/proto_.js
@@ -0,0 +1,16 @@
+//prototype.
+function Animal(owner){
+    this.owner = owner
+    
+
+}
+function Dog(name, color){
+    this.name = name,
+    this.color = color
+}
+
+let a = new Animal("John");
+let b = new Dog("Huskey", "White");
+
+b.__proto__.owner=a.owner;
+console.log(b.owner)
