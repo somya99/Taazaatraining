443fae2de87180ee5237ac84f80ee5bd35d13e38
somya99 <dassomya1999@gmail.com>
2021-03-24 18:35:30 +0530

    DAY9 commit

---
+++ Day 9(Delegate, Event)/CalculatorDelegateDemo/PrintResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDelegateDemo
+{
+    public class PrintResult
+    {
+        public delegate int Result(int num1, int num2);
+        public void GetResult(Result ans, int num1, int num2)
+        {
+            var status = ans(num1, num2);
+            Console.WriteLine("Result is " + status);
+        }
+    }
+}
---
+++ Day 9(Delegate, Event)/CalculatorDelegateDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDelegateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintResult res = new PrintResult();
+            Addition add = new Addition();
+            Subtraction sub = new Subtraction();
+            Multiplication mul = new Multiplication();
+            Division div = new Division();
+            int n1 = 15;
+            int n2 = 5;
+            PrintResult.Result calculate = add.Add;
+            res.GetResult(calculate, n1, n2);
+            calculate += sub.Subtract;
+            res.GetResult(calculate, n1, n2);
+            calculate += mul.Multiply;
+            res.GetResult(calculate, n1, n2);
+            calculate += div.Divide;
+            res.GetResult(calculate, n1, n2);
+            Console.ReadKey(true);
+        }
+    }
+}
