a87e4b22f04a2c80faeca736ed1007945cd454c8
somya99 <dassomya1999@gmail.com>
2021-03-25 18:53:35 +0530

    DAY10 commit

---
+++ Day 10(Message Delivery)/TaskMessage/Agent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMessage
+{
+    public class Agent
+    {
+        public delegate void NotificationHandler(object agent, NotificationEventArgs notificationInfo);
+        public event NotificationHandler notificationChanged;
+        public delegate bool tasksenddelegate(Message m);
+        private bool isTaskCompleted = false;
+        public void doTask(tasksenddelegate task, Message m)
+        {
+            NotificationEventArgs notificationInfo = new NotificationEventArgs(isTaskCompleted);
+            if(isTaskCompleted == false)
+            {
+                if (notificationChanged != null)
+                {
+                    Console.WriteLine("Start");
+                    notificationChanged(this, notificationInfo);
+                }
+            }
+            isTaskCompleted = true;
+            if (isTaskCompleted)
+            {
+                var status = task(m);
+                if(notificationChanged != null)
+                {
+                    notificationChanged(this, notificationInfo);
+                    Console.WriteLine("ty");
+                }
+            }
+        }
+
+    }
+}
