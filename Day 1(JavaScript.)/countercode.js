395f95a4f26568ab0f1c749fe0567c388f276c0d
somya99 <dassomya1999@gmail.com>
2021-03-26 18:04:20 +0530

    15 March Commit

---
+++ Day 1(JavaScript.)/countercode.js
@@ -0,0 +1,15 @@
+function count(){
+    let result = 0;
+    function countIntern(){
+        result++;
+        return result;
+        
+    }
+    return countIntern;
+}
+let a = count();
+console.log(a());
+console.log(a());
+console.log(a());
+console.log(a());
+console.log(a());
