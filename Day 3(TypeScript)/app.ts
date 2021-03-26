a63cde93920a46f456c1c94da428d89f05977483
somya99 <dassomya1999@gmail.com>
2021-03-26 17:56:39 +0530

    17 March commit

--- Day 3(TypeScript)/app.ts
+++ Day 3(TypeScript)/app.ts
@@ -1,4 +1,4 @@
-//app.js
+/*app.js*/
 import { Product } from "./product";
 
 function getProduct(){
--- Day 3(TypeScript)/product.ts
+++ Day 3(TypeScript)/product.ts
@@ -1,3 +1,4 @@
+/*product*/
 export class Product{
     public Id : string;
     public Title : string;
