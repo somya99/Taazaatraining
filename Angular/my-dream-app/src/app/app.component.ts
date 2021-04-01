d410010b15b3c4120cbf4085334f96dda72183bd
somya99 <dassomya1999@gmail.com>
2021-04-01 19:23:45 +0530

    Angular Commit

--- Angular/my-dream-app/src/app/app.component.ts
+++ Angular/my-dream-app/src/app/app.component.ts
@@ -9,16 +9,23 @@ export class AppComponent {
   title = 'Angular Demo';
   showImageChangeDemo = true;
   showProductListDemo = false;
-
+  showMasterDetails = false;
   showimagechangedemo(): void{
     this.showImageChangeDemo = true;
     this.showProductListDemo = false;
+    this.showMasterDetails = false;
   }
 
   showproductlistdemo(): void{
     this.showImageChangeDemo = false;
     this.showProductListDemo = true;
+    this.showMasterDetails = false;
   }
-   
+  
+  showmasterdetails(): void{
+    this.showImageChangeDemo = false;
+    this.showProductListDemo = false;
+    this.showMasterDetails = true;
+  }
   
 }
