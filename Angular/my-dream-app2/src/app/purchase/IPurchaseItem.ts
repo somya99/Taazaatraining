53376541cabb4b9518f4b999c38db7957b94918c
somya99 <65486312+somya99@users.noreply.github.com>
2021-04-01 19:19:33 +0530

    Delete Angular/my-dream-app2 directory

--- Angular/my-dream-app2/src/app/purchase/IPurchaseItem.ts
+++
@@ -1,9 +0,0 @@
-import { IProduct } from "../product/IProduct";
-
-export interface IPurchaseItem{
-    PurchaseID : number,
-    PurchaseName : string,
-    PurchaseDate : string,
-    VendorName : string,
-    Items : IProduct[]
-}
